79a6b8ae9bd185682202d05b30dbc9897bbbe159
Yusuf GÜREL <yusufgurel@outlook.com>
2023-05-30 01:30:38 +0300

    Add files via upload

---
+++ InstaClone/InstaClone2/SettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.swift
+//  InstaClone2
+//
+//  Created by GUREL on 29.05.2023.
+//
+
+import UIKit
+import Firebase
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+    @IBAction func signOutClicked(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toViewController", sender: nil)
+            
+        } catch {
+            print("Error")
+        }
+        
+        
+    }
+    
+    
+
+
+
+}
